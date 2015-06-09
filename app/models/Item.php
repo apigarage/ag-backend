a8cbaf45e6913b8712682e4a82214d2a49a7d498
Chinmay Patel <chinmay@chinmay.ca>
2015-06-09 00:10:19 -0400

    Adding project_id to the auto-fillable field.

--- app/models/Item.php
+++ app/models/Item.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 
 class Item extends Eloquent {
 
-    protected $fillable = ['author_id', 'uuid', 'collection_id','name','description','url','method','headers','data'];
+    protected $fillable = ['author_id','uuid','collection_id','project_id','name','description','url','method','headers','data'];
     protected $table = 'items';
 
     public function getHeaderAttribute($value)
