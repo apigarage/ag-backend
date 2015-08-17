be61bbf986e55f3ca20d19461839d80b83ab41fc
Gamal Tawaf <ja.tawaf@gmail.com>
2015-08-16 22:54:41 -0400

    Fixing command

--- app/commands/MigrateEnviromentVars.php
+++ app/commands/MigrateEnviromentVars.php
@@ -41,18 +41,41 @@ class MigrateEnviromentVars extends Command {
     for($i = 0 ; $i < count($all_env_vars) ; $i++)
     {
       $environment = Environment::find($all_env_vars[$i]['environment_id']);
+      // adds the project key
       $key_exists = ProjectKey::where('name' ,'=' ,$all_env_vars[$i]['name'])
                               ->where('project_id', '=', $environment->project_id)->first();
-
-      if(!empty($all_env_vars[$i]['name']) && !empty($environment->name))
+      // if environment does not exist we can not do anything 
+      if(!empty($environment))
       {
-        if(empty($key_exists))
+        if(!empty($all_env_vars[$i]['name']) && !empty($environment->name))
         {
-          $key_exists = new ProjectKey();
-          $key_exists->name = trim($all_env_vars[$i]['name']);
-          $key_exists->project_id = trim($environment->project_id);
-          $key_exists->save();
+          if(empty($key_exists))
+          {
+            $key_exists = new ProjectKey();
+            $key_exists->name = trim($all_env_vars[$i]['name']);
+            $key_exists->project_id = trim($environment->project_id);
+            $key_exists->save();
+          }
         }
+        // adds association to all environments dessicated with that project
+        $project_enviroments = Environment::where('project_id', '=', $environment->project_id)->get();
+        if(!empty($project_enviroments) && !empty($key_exists)) 
+        {
+          foreach ($project_enviroments as $project_enviroment) 
+          { 
+            $environment_key = ProjectKeyEnvironment::where('environment_id', '=', $project_enviroment->id)
+                                          ->where('project_key_id', '=', $key_exists->id)->first();
+            // if no ProjectKeyEnvironment exists create one with empty value
+            if(empty($environment_key))
+            {
+              $environment_key = new ProjectKeyEnvironment();
+              $environment_key->environment_id =  $project_enviroment->id;
+              $environment_key->project_key_id =  $key_exists->id;
+              $environment_key->save();
+            }
+          }
+        }
+        // fill value for current environment var 
         $environment_key = ProjectKeyEnvironment::where('environment_id', '=', $all_env_vars[$i]['environment_id'])
                                           ->where('project_key_id', '=', $key_exists->id)
                                           ->where('value', '=', $all_env_vars[$i]['value'])->first();
