61daf426c9abb7570f9cd6e8cdd0ea320de44fb6
Chinmay Patel <chinmay@chinmay.ca>
2015-10-19 18:31:28 -0400

    Updating share email context.

--- app/models/Project.php
+++ app/models/Project.php
@@ -99,30 +99,33 @@ class Project extends Model {
   }
 
   public function sendSignUpEmailWithProjectInvitation($to_email){
+    $current_resource_owner = Authorizer::getResourceOwnerId();
+    $user = User::find($current_resource_owner)->toArray();
+
+    // Build the Subject Line
+    $params['title'] = "You are now part of the project -- " . $this->name;
+    $params['user'] = $user ;
+    $params['project'] = $this->toArray();
+
+
+    // Do not create an invitation, if it already exists.
     $shared_project = ProjectInvitation::where('email', '=', $to_email)
-                                      ->where('project_id', '=', $this->id )->first();
-    // if there exists a project shared then don't share
-    if(empty($share_project))
+      ->where('project_id', '=', $this->id )->first();
+    if(empty($shared_project))
     {
-
-      $current_resource_owner = Authorizer::getResourceOwnerId();
-      $user = User::find($current_resource_owner)->toArray();
-      $params['user'] = $user ;
-      $params['project'] = $this->toArray() ;
-      $params['title'] ='Shared A Project' ;
-
-      $share_project = new ProjectInvitation();
-      $share_project->from_user = $user['id'] ;
-      $share_project->project_id = $this->id ;
-      $share_project->email = $to_email ;
-      $share_project->save();
-
-      $params['content'] = View::make('emails.ShareSignup' , array( 'params' => $params));
-      Mail::send('emails.master', ['params' => $params], function($message) use($to_email)
-      {
-         $message->to($to_email)->subject('Project Shared With you');
-      });
+      $shared_project = new ProjectInvitation();
+      $shared_project->from_user = $user['id'] ;
+      $shared_project->project_id = $this->id ;
+      $shared_project->email = $to_email ;
+      $shared_project->save();
     }
+
+    // Send the email regardless of it exists or not.
+    $params['content'] = View::make('emails.ShareSignup' , array( 'params' => $params));
+    Mail::send('emails.master', ['params' => $params], function($message) use($to_email, $params)
+    {
+      $message->to($to_email)->subject($params['title']);
+    });
   }
 
   // gets all users that are associated with this project
