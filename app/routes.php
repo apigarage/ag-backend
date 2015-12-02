7f2e35fd5b68cdc12af8ec91b40de69dfd0727aa
Chinmay Patel <chinmay@chinmay.ca>
2015-12-01 21:53:22 -0500

    Get All Project Responses.

--- app/routes.php
+++ app/routes.php
@@ -11,6 +11,7 @@ Route::get('/', function() {
 
 Route::resource('emails', 'EmailsController', ['only' =>['index']]);
 
+
 /**
  * Publicly Avaialable Routes.
  * TODO - Please lock this with client-id and secret.
@@ -40,6 +41,7 @@ Route::group(array('prefix' => 'api','before' => 'oauth'), function()
   Route::resource('collections', 'CollectionsController');
   Route::resource('projects.environments', 'EnvironmentsController');
   Route::resource('projects.keys.environments', 'PorjectKeyEnvironmentController');
+  Route::resource('projects.responses', 'ProjectResponsesController');
   Route::resource('environments.vars', 'EnvironmentVarsController');
   Route::resource('projects.keys', 'PorjectKeyController');
   Route::resource('items', 'ItemsController');
